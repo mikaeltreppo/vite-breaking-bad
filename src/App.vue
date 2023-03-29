535aec75c6fcb7ca3b953af642f868c5f7995d4a
mikaeltreppo <mikaeltreppo@outlook.com>
2023-03-29 17:24:32 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<script>
+import axios from 'axios';
+import {store} from '../store.js';
+import MyHeader from './components/MyHeader.vue';
+import MyMain from'./components/MyMain.vue'
+export default {
+    data() {
+      return {
+        MyHeader,
+        MyMain,
+        store
+      }
+    },
+    methods: {
+      getCards() {
+        axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?format=Speed Duel')
+        .then(response => {
+            console.log(response)
+    
+        });
+      }
+    },
+    created() {
+      this.getCards();
+    }
+  }
+
+
+
+</script>
+
+<template>
+  <MyHeader></MyHeader>
+  <MyMain></MyMain>
+
+</template>
+
+<style scoped></style>
+
+
+
+  
---
+++ src/components/MyHeader.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="header">
+  <img src="https://occ.a.nflxso.net/dnm/api/v6/LmEnxtiAuzezXBjYXPuDgfZ4zZQ/AAAABdpYalFU9SD9K6dNwu9SlKsNaQ3AIGWFrN_uDy6R7N3TS5DHIatIETtgeqwC0kM01zJyor2znGql_ixTWzVueA-GllV7Hn03ygETN_Nd7oZA.png?r=81f"
+            alt="">
+    </div>
+</template>
+<style>
+.header {
+    height: 60px;
+    background-color: white;
+
+}
+</style>
+<script>
+</script>
